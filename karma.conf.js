33cf7ce59740f079176dcd162ecfb5cb8bc889c5
skarunamoorthy <skarunamoorthy@resqsoft.com>
2021-09-30 19:37:06 +0530

    Update eureka solutions

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/EurekaSolutions'),
+      dir: require('path').join(__dirname, './coverage/EurekaSolutionsUI'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'EurekaSolutions'`, () => {
+  it(`should have as title 'EurekaSolutionsUI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('EurekaSolutions');
+    expect(app.title).toEqual('EurekaSolutionsUI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('EurekaSolutions app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('EurekaSolutionsUI app is running!');
   });
 });
